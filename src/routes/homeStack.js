09dd45da997915b045335b983e249114ddca185d
Andrew Kim <80931105+andki6@users.noreply.github.com>
2023-10-18 22:12:22 -0400

    Andrew (#3)
    * added bottom tabs, refactored code
    
    * added bottom tabs and stack navigation and profile
    
    ---------
    
    Co-authored-by: Andrew Kim <andrewkim@gmail.com>

--- src/routes/homeStack.js
+++ src/routes/homeStack.js
@@ -1,19 +1,16 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
 //import screens here
-import {
-    LoginScreen,
-  
-  } from '../screens'
+import Login from "../screens/LoginScreen.js";
 
 const screens = {
-    // Home: {
-    //     screen: //home screen
-    // },
-    LogIn: {
-        screen: LoginScreen
-    }
-}
+  // Home: {
+  //     screen: //home screen
+  // },
+  LogIn: {
+    screen: Login,
+  },
+};
 
 const HomeStack = createStackNavigator(screens);
 
---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const RegisterScreen = ({ navigation }) => {
+  function registerUser(email, password) {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log("yippee", user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
+  }
+
+  let email = "";
+  let password = "";
+
+  function setEmail(val) {
+    email = val;
+  }
+
+  function setPassword(val) {
+    password = val;
+  }
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(val) => setEmail(val)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={(val) => setPassword(val)}
+      />
+      <Button title="Sign Up" onPress={() => registerUser(email, password)} />
+      <Text>Already have an account?</Text>
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate("LoginScreen")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default RegisterScreen;
