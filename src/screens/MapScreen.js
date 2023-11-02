71681f536bbf596c4a6ac319789e27365342ba06
rdhadda123 <rdhadda@stevens.edu>
2023-11-02 12:41:26 -0400

    Enabling location

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,11 +1,27 @@
-import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, Text, Button } from "react-native";
+import * as Location from "expo-location";
 
 const MapScreen = () => {
+  const [locationPermissionGranted, setLocationPermissionGranted] = useState(false);
+
+  useEffect(() => {
+    // Call handleMapTabClick when the component is mounted (page is opened)
+    const requestLocationPermission = async () => {
+      const granted = await Location.requestForegroundPermissionsAsync();
+      if (granted) {
+        setLocationPermissionGranted(true);
+      }
+    };
+
+    requestLocationPermission();
+  }, []); // The empty dependency array ensures this effect runs once
+
   return (
     <SafeAreaView>
       <View>
         <Text>Map</Text>
+        {locationPermissionGranted && <Text>Location Permission Granted!</Text>}
       </View>
     </SafeAreaView>
   );
