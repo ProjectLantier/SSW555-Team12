d0789b5575bd98608254209dd670c8047b403121
Andrew Kim <andrewkim@gmail.com>
2023-11-15 18:13:40 -0500

    created initialized data for firebase realtime database and fetched and displayed location data

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
 import "@firebase/auth";
 
 const firebaseConfig = {
@@ -14,5 +14,83 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+
+const locations = [
+  {
+    id: 0,
+    name: "Stevens Insitute of Technology",
+    latitude: 40.7448,
+    longitude: -74.0256,
+    latitudeDelta: 0.04,
+    longitudeDelta: 0.02,
+    description: "cheap college",
+    visited: false,
+  },
+  {
+    id: 1,
+    name: "Benny Tudinos",
+    latitude: 40.74424,
+    longitude: -74.02912,
+    latitudeDelta: 0.04,
+    longitudeDelta: 0.02,
+    description: "pizza meme",
+    visited: false,
+  },
+  {
+    id: 2,
+    name: "Hoboken Terminal",
+    latitude: 40.7349,
+    longitude: -74.0291,
+    latitudeDelta: 0.04,
+    longitudeDelta: 0.02,
+    description: "fun times...",
+    visited: false,
+  },
+];
+
+const badges = [
+  {
+    id: 0,
+    name: "Noobie",
+    description: "Awarded for your first location visited!",
+  },
+  {
+    id: 1,
+    name: "Explorer",
+    description: "Awarded for visiting three locations!",
+  },
+  {
+    id: 2,
+    name: "Adventurer",
+    description: "Awarded for visiting 10 locations!",
+  },
+  {
+    id: 3,
+    name: "Foodie Adventurer",
+    description: "Awarded for visiting 5 food locations!",
+  },
+  {
+    id: 4,
+    name: "Time Traveler",
+    description: "Awarded for visiting 5 hisorical sites!",
+  },
+];
+
+function writeLocationData(locations) {
+  for (const location of locations) {
+    const reference = ref(db, `locations/${location.id}`);
+    set(reference, location);
+  }
+}
+
+function writeBadgeData(badges) {
+  for (const badge of badges) {
+    const reference = ref(db, `badges/${badge.id}`);
+    set(reference, badge);
+  }
+}
+
+writeLocationData(locations);
+writeBadgeData(badges);
 
 export { app, db };
