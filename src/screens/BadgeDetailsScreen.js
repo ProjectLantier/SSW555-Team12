26944a4cc9adf3f0e3301aefa2bc22f9d48feee5
rdhadda123 <rdhadda@stevens.edu>
2023-11-16 20:14:36 -0500

    Merge branch 'main' of https://github.com/ProjectLantier/SSW555-Team12 into rishabh

--- src/screens/BadgeDetailsScreen.js
+++ src/screens/BadgeDetailsScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { SafeAreaView, View, Text, StyleSheet } from "react-native";
 import BadgeAndDetailsStyle from "../styles/BadgeAndDetailsStyle";
 
+const styles = BadgeAndDetailsStyle
+
 const BadgeDetailsScreen = ({ route }) => {
   const { badge } = route.params;
   //dummy info for now
