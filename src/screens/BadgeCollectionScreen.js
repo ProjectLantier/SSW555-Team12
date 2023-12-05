eda53fb90c90031366dae5deed785e86b05b6229
rdhadda123 <rdhadda@stevens.edu>
2023-12-05 10:26:20 -0500

    Adding points

--- src/screens/BadgeCollectionScreen.js
+++ src/screens/BadgeCollectionScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import {
   View,
   Text,
@@ -12,33 +12,47 @@ import {
 const BadgeCollectionScreen = ({ navigation }) => {
   //add image to badge later
   //grab these from database
+  const [totalPoints, setTotalPoints] = useState(0);
   const badges = [
     {
       id: 1,
       name: "Noobie",
       description: "Awarded for your first location visited!",
+      points: 1,
     },
     {
       id: 2,
       name: "Explorer",
       description: "Awarded for visiting three locations!",
+      points: 2,
     },
     {
       id: 3,
       name: "Adventurer",
       description: "Awarded for visiting 10 locations!",
+      points: 4,
     },
     {
       id: 4,
       name: "Foodie Adventurer",
       description: "Awarded for visiting 5 food locations!",
+      points: 3,
     },
     {
       id: 5,
       name: "Time Traveler",
-      description: "Awarded for visiting 5 hisorical sites!",
+      description: "Awarded for visiting 5 historical sites!",
+      points: 3,
     },
   ];
+
+  useEffect(() => {
+    let points = 0;
+    badges.forEach(badge => {
+      points += badge.points;
+    });
+    setTotalPoints(points);
+  })
 
   return (
     <SafeAreaView style={styles.wrapper}>
@@ -48,7 +62,7 @@ const BadgeCollectionScreen = ({ navigation }) => {
       
       <View style={styles.container}>
         <Text style={styles.heading}>Badges</Text>
-        <Text style={{marginTop: 10, fontWeight: "bold"}}>Adventurer Level: {badges.length}</Text>
+        <Text style={{marginTop: 10, fontWeight: "bold"}}>Adventurer Level: {totalPoints}</Text>
       </View>
       <View style={styles.badges}>
         {badges.map((badge) => {
@@ -64,6 +78,7 @@ const BadgeCollectionScreen = ({ navigation }) => {
                 <Text>
                   <Text style={styles.badgeHead}>Badge {badge.id}: </Text>
                   <Text style={styles.badgeName}>{badge.name}</Text>
+                  <Text style={[styles.badgePoints, { alignSelf: "flex-end" }]}>Points: {badge.points}</Text>
                 </Text>
                 <Text style={styles.badgeDescription}> {badge.description}</Text>
 
@@ -118,7 +133,11 @@ const styles = StyleSheet.create({
   },
   badgeInformation: {
     padding: 10,
-    marginBottom: 5
+    marginBottom: 5,
+  },
+  badgeInfoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   badgeHead: {
     fontSize: 24,
@@ -128,6 +147,13 @@ const styles = StyleSheet.create({
   badgeName: {
     fontSize: 20,
   },
+  badgePoints: {
+    alignSelf: "flex-end",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#033dfc",
+  },
+  
   badgeDescription: {
     fontSize: 18,
     color: "#788eec",
